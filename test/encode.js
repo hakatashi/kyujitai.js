fb4b26173549689396cbc3f8b0f4564c5e8f5ffd
hakatashi <hakatasiloving@gmail.com>
2014-09-07 03:16:06 +0900

    Update some entries and add test

--- test/encode.js
+++ test/encode.js
@@ -13,4 +13,13 @@ describe('encode', function () {
 		kyujitai.encode('連合国軍最高司令官総司令部').should.equal('聯合國軍最高司令官總司令部');
 		kyujitai.encode('日本戦没学生記念会').should.equal('日本戰歿學生記念會');
 	});
+
+	it('should correctly classify same kanji into different kyujitai according to contexts', function () {
+		kyujitai.encode('弁論大会').should.equal('辯論大會');
+		kyujitai.encode('弁償金額').should.equal('辨償金額');
+		kyujitai.encode('弁膜症患者').should.equal('瓣膜症患者');
+		kyujitai.encode('弁髪令').should.equal('辮髮令');
+		kyujitai.encode('合弁会社').should.equal('合辦會社');
+		kyujitai.encode('弁髦').should.equal('弁髦');
+	});
 });
