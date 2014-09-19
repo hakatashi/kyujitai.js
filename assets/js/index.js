730445541cc58910ff001260156f91e8ff0206a4
hakatashi <hakatasiloving@gmail.com>
2014-09-19 10:03:05 +0900

    Make logo image hoverable

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,10 @@
+// http://stackoverflow.com/questions/476679/
+function preload(arrayOfImages) {
+    $(arrayOfImages).each(function(){
+        $('<img/>')[0].src = this;
+    });
+}
+
 $(document).ready(function () {
 	$.vegas('slideshow', {
 		backgrounds: [
@@ -8,7 +15,19 @@ $(document).ready(function () {
 		delay: 10000,
 		preload: true
 	});
+
 	$.vegas('overlay', {
 		src:'assets/vegas/overlays/04.png'
 	});
+
+	preload([
+		'assets/img/logo_hover.png'
+	]);
+
+	var $logo = $('img#logo');
+	$('#container').hover(function () {
+		$logo.attr({src: 'assets/img/logo_hover.png'});
+	}, function () {
+		$logo.attr({src: 'assets/img/logo.png'});
+	})
 });
