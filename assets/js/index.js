ad6ee49e0c7a274aa8465ab12ab8aa3ebae6043c
hakatashi <hakatasiloving@gmail.com>
2014-09-19 09:51:34 +0900

    Update background of Pages

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,12 @@
 $(document).ready(function () {
-	$.vegas({
-		src:'assets/img/background01.jpg'
+	$.vegas('slideshow', {
+		backgrounds: [
+			{src: 'assets/img/background01.jpg', fade: 1000},
+			{src: 'assets/img/background02.jpg', fade: 1000},
+			{src: 'assets/img/background03.jpg', fade: 1000}
+		],
+		delay: 10000,
+		preload: true
 	});
 	$.vegas('overlay', {
 		src:'assets/vegas/overlays/04.png'
